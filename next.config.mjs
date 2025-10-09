6277e198db6a78f3fdf5aca98456481416d0811b
ataur65 <ataur.rahman6846@gmail.com>
2025-10-10 00:12:14 +0600

    build: include postcss and tailwind configs in build output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': ['./postcss.config.js', './tailwind.config.js'],
+    },
+  },
 };
 
 export default nextConfig;
